e8839b988363f75c68c1f9f583f3bea091a6a5a5
Rafael Azevedo <79703126+rrafaelazevedo@users.noreply.github.com>
2024-03-22 15:32:22 -0300

    converter_metragem

---
+++ converter_metragem.py
@@ -0,0 +1,4 @@
+m = float(input('Digite o valor numérico da metragem: '))
+cm = (m * 100)
+mm = (m * 1000)
+print('O valor de {}m, pode ser convertido em {}cm e  também para {}mm.'.format(m, cm, mm))
